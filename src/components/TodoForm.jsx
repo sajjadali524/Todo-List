cab9c4052071aff5cb34cf479071c1138afbce9a
Sajjad Ali <sajadalisk958@gmail.com>
2025-07-06 03:02:46 -0700

    completed

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,23 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoList from './TodoList';
 
 const TodoForm = () => {
-    const [todos, setTodos] = useState([]);
-    const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
 
-    const addTodoToList = () => {
-      setTodos([...todos, todoText]);
-      setTodoText("");
-    };
+  useEffect(() => {
+    const getTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(getTodos);
+  }, []);
+
+  const addAndUpdateTodo = () => {
+    if(!todoText.trim()) return;
+    let updatedTodos;
+    if(editIndex !== null) {
+      // update text
+      updatedTodos = todos.map((todo, index) => {
+      return index === editIndex ? {...todo, text: todoText, isCompleted: false } : todo;
+      })
+    }else {
+      updatedTodos = [{text: todoText, isCompleted: false}, ...todos]
+    }
+    
+    setTodos(updatedTodos)
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    setTodoText("");
+    setEditIndex(null);
+  };
+
+  const handleEdit = (index) => {
+    setTodoText(todos[index].text);
+    setEditIndex(index);
+  };
 
   return (
-    <div className="w-1/2 bg-white shadow-md rounded-md p-6 space-y-5">
+    <div className="lg:w-1/2 w-full bg-white shadow-md rounded-md p-6 space-y-5">
         <div className="flex items-center justify-center gap-3 w-full">
-            <input type="text" value={todoText} onChange={(e) => setTodoText(e.target.value)} placeholder="Add a new todo" className="w-full outline-none rounded-md px-5 py-2 border border-gray-100" />
-            <button className="bg-blue-500 text-white rounded-md px-4 py-2 cursor-pointer" onClick={() => addTodoToList()}>Add</button>
+            <input type="text" placeholder="Add a new todo" value={todoText} onChange={(e) => setTodoText(e.target.value)} className="w-full outline-none rounded-md px-5 py-2 border border-gray-100" />
+            <button className="bg-blue-500 text-white rounded-md px-4 py-2 cursor-pointer" onClick={addAndUpdateTodo}>{editIndex !== null ? "Update" : "Add"}</button>
         </div>
-
-        <TodoList todos={todos} />
+        <TodoList todos={todos} setTodos={setTodos} onEdit={handleEdit} />
     </div>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoList = ({todos = []}) => {
+const TodoList = ({todos, setTodos, onEdit}) => {
+  const deleteTodo = (i) => {
+    const updatedTodo = todos.filter((todo, index) => index !== i);
+    setTodos(updatedTodo);
+    localStorage.setItem("todos", JSON.stringify(updatedTodo))
+  };
+
+  const completedTodo = (i) => {
+    const updatedTodo = todos.map((todo, index) => {
+      if(index === i) {
+        return {...todo, isCompleted: true}
+      }
+      return todo;
+    });
+    setTodos(updatedTodo);
+    localStorage.setItem("todos", JSON.stringify(updatedTodo))
+  };
+
   return (
     <div>
-        <h2 className="text-lg font-semibold mb-4">Todo List</h2>
-        <ul>
-            {
-              todos.map((todo, index) => {
-                return(
-                  <li key={index}>{todo}</li>
-                )
-              })
-            }
-        </ul>
+      <h2 className="text-lg font-semibold mb-4">Todo List</h2>
+      <ul className="space-y-2">
+        {todos.map((todo, index) => (
+          <li key={index} className="flex items-center justify-between">
+            <span className={todo.isCompleted ? "text-red-500 line-through" : "text-black"}>{todo.text}</span>
+            <div className="flex items-center gap-2">
+              <button className="bg-blue-500 px-2 py-1 rounded-md text-white" onClick={() => onEdit(index)}>Edit</button>
+              <button className="bg-red-500 px-2 py-1 rounded-md text-white" onClick={() => deleteTodo(index)}>Delete</button>
+              <button className="bg-green-500 px-2 py-1 rounded-md text-white" onClick={() => completedTodo(index)}>Complete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default TodoList;
